76f2bf4bf45750babab16f5cabe11b0e0456eacc
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-03-10 16:35:55 -0600

    Finished a rough starting skeleton

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var exphbs  = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +12,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+const hbs = exphbs.create({
+  extname: 'hbs',
+  defaultLayout: 'layout.hbs',
+  layoutsDir   : 'views/layouts',
+  partialsDir  : 'views/partials'
+});
+
+app.engine('hbs', hbs.engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+//Determine if faster to have local images or github images
+
 /* GET home page. */
 router.get('/', function(req, res, next) {//https://web.archive.org/web/20120709052134/http://www.runescape.com:80/title.ws
-  res.render('index', {});
+  res.render('index', { layouts: 'layout.hbs'});
 });
 
 router.get('/news1', function(req, res, next) {//https://web.archive.org/web/20120704185400/http://services.runescape.com/m=news/behind-the-scenes-july
-  res.render('news-example1', { layout: 'layout-news.hbs' });
+  res.render('pages/news-example1', { layout: 'layout-news.hbs' });
+});
+
+router.get('/media', function(req, res, next) {//remove
+  res.redirect('/', {});
+});
+
+router.get('/game-guide', function(req, res, next) {//remove
+  res.render('pages/game-guide', { layout: 'layout-gameguide.hbs' });
+});
+
+router.get('/beginners-guide', function(req, res, next) {//remove getting started, what's next
+  res.render('pages/beginners-guide.hbs', { layout: 'layout-beginnersguide.hbs' });
 });
 
 router.get('/world', function(req, res, next) {//https://web.archive.org/web/20120731001805/http://www.runescape.com:80/world_locations.ws
-  res.render('news-example1', { layout: 'layout-news.hbs' });
+  res.render('pages/world/world', { layout: 'layout-world.hbs' });
 });
+
+// router.get('/world/lumbridge', function(req, res, next) {//https://web.archive.org/web/20120731001805/http://www.runescape.com:80/world_locations.ws
+//   res.render('pages/world', { layout: 'layout-world.hbs' });
+// });
+
+router.get('/rs-wiki', function(req, res, next) {
+  res.render('world', { layout: 'layout-world.hbs' });
+});
+
+
+router.get('/downloads', function(req, res, next) { https://web.archive.org/web/20120718053538/http://www.runescape.com/downloads.ws
+  res.render('pages/downloads', { layout: 'layout-downloads.hbs' });
+});
+
+router.get('/customer-support', function(req, res, next) { https://web.archive.org/web/20120620024434/http://services.runescape.com:80/m=rswiki/en/Customer_Support
+    res.render('pages/customer-support', { layout: 'layout-customersupport.hbs' });
+});
+
+router.get('/hall-of-heroes', function(req, res, next) { https://web.archive.org/web/20120620024338/http://services.runescape.com/m=hiscore/heroes.ws
+    res.render('pages/hall-of-heroes', { layout: 'layout-hallofheroes.hbs' });
+});
+
+router.get('/highscores', function(req, res, next) { https://web.archive.org/web/20120608083454/http://services.runescape.com:80/m=hiscore/overall.ws?category_type=0&table=0
+    res.render('pages/highscores', { layout: 'layout-highscores.hbs' });
+});
+
 
 module.exports = router;
