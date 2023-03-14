3b697e0fd9e3487aec6f071d1d5c08b3b5643c87
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-03-13 21:51:57 -0500

    Markdown editor works

--- routes/articles.js
+++ routes/articles.js
@@ -46,18 +46,18 @@ router.get("/admin", (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-    res.render('articles/new', { article: new Article(), layout: "layout-writenews" })
+    res.render('articles/new', { article: new Article(), layout: "layout-writenews", news: true })
 })
 
 router.get('/edit/:id', async (req, res) => {
     const article = await Article.findById(req.params.id)
-    res.render('articles/edit', { article: article, isModerator: isModerator(req), layout: "layout-writenews" })
+    res.render('articles/edit', { article: article, isModerator: isModerator(req), layout: "layout-writenews", news: true })
 })
 
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug })
     if (article == null) res.redirect('/')
-    res.render('articles/show', { article: article, isModerator: isModerator(req), layout: "layout-writenews" })
+    res.render('articles/show', { article: article, isModerator: isModerator(req), layout: "layout-writenews", news: true })
 })
 
 router.post('/', async (req, res, next) => {
@@ -86,68 +86,9 @@ function saveArticleAndRedirect(path) {
             res.redirect(`/articles/${article.slug}`)
         } catch (e) {
             console.log(e)
-            res.render(`articles/${path}`, { article: article, isModerator: isModerator(req), layout: "layout-writenews" })
+            res.render(`articles/${path}`, { article: article, isModerator: isModerator(req), layout: "layout-writenews", news: true })
         }
     }
 }
-
-// router.get("/admin", (req, res) => {
-//     authenticate(req, res);
-// });
-//
-// router.get('/edit/:id', async (req, res) => {
-//     const article = await Article.findById(req.params.id)
-//     res.render('pages/write-news/edit', { article: article, isModerator: isModerator(req),
-//         uploadToken: auth.uploadToken, layout: 'layout-writenews', news: true })
-// })
-//
-// router.get('/:slug', async (req, res) => {
-//     const article = await Article.findOne({ slug: req.params.slug })
-//     if (article == null) res.redirect('/')
-//     res.render('pages/write-news/show', { article: article, isModerator: isModerator(req), layout: 'layout-writenews', news: true })
-// })
-//
-// router.get('/', async (req, res) => {
-//     const articles = await Article.find().sort({ createdAt: 'desc' });
-//     res.render('pages/write-news/index', { articles: articles, isModerator: isModerator(req), layout: 'layout-writenews', news: true });
-// })
-//
-//
-//
-// router.get('/new', (req, res) => {
-//     res.render('pages/write-news/new', { article: new Article(), isModerator: isModerator(req),
-//         uploadToken: auth.uploadToken, layout: 'layout-writenews', news: true })
-// })
-//
-// router.post('/', async (req, res, next) => {
-//     req.article = new Article()
-//     next()
-// }, saveArticleAndRedirect('new'))
-//
-// router.put('/:id', async (req, res, next) => {
-//     console.log("putted")
-//     req.article = await Article.findById(req.params.id)
-//     next()
-// }, saveArticleAndRedirect('edit'))
-//
-// router.delete('/:id', async (req, res) => {
-//     await Article.findByIdAndDelete(req.params.id)
-//     res.redirect('/')
-// })
-//
-// function saveArticleAndRedirect(path) {
-//     return async (req, res) => {
-//         let article = req.article
-//         article.title = req.body.title
-//         article.description = req.body.description
-//         article.markdown = req.body.markdown
-//         try {
-//             article = await article.save()
-//             res.redirect('/news-posts/'+ article.slug)
-//         } catch (e) {
-//             res.render('pages/write-news/' + path, { article: article, layout: 'layout', news: true })
-//         }
-//     }
-// }
 
 module.exports = router
